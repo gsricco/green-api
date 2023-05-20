208c61503b4d86b7613e577f9a25b41b0f230b9a
sergio <gsricco@yandex.ru>
2023-05-20 15:44:37 +0300

    init: create app and add .idea to gitignore

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './App.module.css'
+
+
+function App() {
+    return (
+        <div className={styles.app}>
+            <div>GREEN_API</div>
+        </div>
+    );
+}
+
+export default App;
